1267831817bfc1183521a876f683931552afa45d
root <root@LAPTOP-Q79RGTDP>
2023-07-16 22:00:41 +0800

    commit lock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,14 +21,31 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU]; // 为每个 CPU 分配独立的 freelist，并用独立的锁保护它。lab8-1
+
+//lab8-1(begin)
+char* kmem_lock_names[] = {
+  "kmem_cpu_0",
+  "kmem_cpu_1",
+  "kmem_cpu_2",
+  "kmem_cpu_3",
+  "kmem_cpu_4",
+  "kmem_cpu_5",
+  "kmem_cpu_6",
+  "kmem_cpu_7",
+};//lab8-1(end)
+
+
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) { // 初始化所有锁 lab8-1
+    initlock(&kmem[i].lock, kmem_lock_names[i]);
+  }
   freerange(end, (void*)PHYSTOP);
 }
+
 
 void
 freerange(void *pa_start, void *pa_end)
@@ -56,10 +73,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();//lab8-1
+
+  int cpu = cpuid();//lab8-1
+
+  acquire(&kmem[cpu].lock);//lab8-1
+  r->next = kmem[cpu].freelist;//lab8-1
+  kmem[cpu].freelist = r;//lab8-1
+  release(&kmem[cpu].lock);//lab8-1
+
+  pop_off();//lab8-1
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +93,39 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();//lab8-1
+
+  int cpu = cpuid();//lab8-1
+
+  acquire(&kmem[cpu].lock);//lab8-1
+
+  //lab8-1(begin)
+  if (!kmem[cpu].freelist) { // no page left for this cpu
+    int steal_left = 64; // steal 64 pages from other cpu(s)
+    for (int i = 0; i < NCPU; i++) {
+      if (i == cpu) continue; // no self-robbery
+      acquire(&kmem[i].lock);
+      struct run* rr = kmem[i].freelist;
+      while (rr && steal_left) {
+        kmem[i].freelist = rr->next;
+        rr->next = kmem[cpu].freelist;
+        kmem[cpu].freelist = rr;
+        rr = kmem[i].freelist;
+        steal_left--;
+      }
+      release(&kmem[i].lock);
+      if (steal_left == 0) break; // done stealing
+    }
+  }
+//lab8-1(end)
+
+
+  r = kmem[cpu].freelist;//lab8-1
+  if (r)
+    kmem[cpu].freelist = r->next;//lab8-1
+  release(&kmem[cpu].lock);//lab8-1
+
+  pop_off();//lab8-1
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
